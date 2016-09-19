c87bdf5c343a9fd5f23a24cf8de5361f64406b6e
Spencer Carli <spencercarli@gmail.com>
2016-09-18 19:34:06 -0500

    Setup tabs

--- app/index.js
+++ app/index.js
@@ -1,23 +1,73 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
 import { Router, Scene } from 'react-native-router-flux';
 
+// New Imports
 import ScarletScreen from './ScarletScreen';
 import GrayScreen from './GrayScreen';
+import BlueScreen from './BlueScreen';
+import MaizeScreen from './MaizeScreen';
+import GoldScreen from './GoldScreen';
+import BlackScreen from './BlackScreen';
+
+// Simple component to render something in place of icon
+const TabIcon = ({ selected, title }) => {
+  return (
+    <Text style={{color: selected ? 'red' :'black'}}>{title}</Text>
+  );
+}
 
 const App = () => {
   return (
     <Router>
       <Scene key="root">
-        <Scene key="scarlet"
-          component={ScarletScreen}
-          title="Scarlet"
-          initial
-        />
+        {/* Tab Container */}
         <Scene
-          key="gray"
-          component={GrayScreen}
-          title="Gray"
-        />
+          key="tabbar"
+          tabs={true}
+          tabBarStyle={{ backgroundColor: '#FFFFFF' }}
+        >
+          {/* Tab and it's scenes */}
+          <Scene key="osu" title="OSU" icon={TabIcon}>
+            <Scene key="scarlet"
+              component={ScarletScreen}
+              title="Scarlet"
+            />
+            <Scene
+              key="gray"
+              component={GrayScreen}
+              title="Gray"
+            />
+          </Scene>
+
+          {/* Tab and it's scenes */}
+          <Scene key="um" title="UM" icon={TabIcon}>
+            <Scene
+              key="blue"
+              component={BlueScreen}
+              title="Blue"
+            />
+            <Scene
+              key="maize"
+              component={MaizeScreen}
+              title="Maize"
+            />
+          </Scene>
+
+          {/* Tab and it's scenes */}
+          <Scene key="vu" title="VU" icon={TabIcon}>
+            <Scene
+              key="gold"
+              component={GoldScreen}
+              title="Gold"
+            />
+            <Scene
+              key="black"
+              component={BlackScreen}
+              title="Black"
+            />
+          </Scene>
+        </Scene>
       </Scene>
     </Router>
   );
